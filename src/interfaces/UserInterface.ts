9ea3f37b12d6d19b37f97b978fb388f5c3e60b3a
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2022-10-13 16:42:55 +0300

    create user Model

--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -1,1 +1,9 @@
-export {}
+import mongoose from 'mongoose'
+
+export default interface UserInterface {
+  id: string
+  name: string
+  email: string
+  password: string
+  slides: [{ _id: mongoose.Types.ObjectId; title: string; link: string }]
+}
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,1 +1,36 @@
-export {}
+import mongoose from 'mongoose'
+import UserInterface from '../interfaces/UserInterface'
+const Schema = mongoose.Schema
+const userSchema = new Schema<UserInterface>({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  slides: [
+    {
+      _id: {
+        type: mongoose.Types.ObjectId,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      link: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+})
+const Uesr = mongoose.model('User', userSchema)
+export default Uesr

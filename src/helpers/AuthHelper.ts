b0e3737be881ddbb99b9dee61fcd318688e75b4f
fadezak100 <fadyzaqut@gmail.com>
2022-10-16 10:53:37 +0300

    init commit for verifyaccess token

--- src/helpers/AuthHelper.ts
+++ src/helpers/AuthHelper.ts
@@ -1,1 +1,26 @@
-export {}
+import { Request, Response, NextFunction } from 'express'
+import jwt, { Secret, JwtPayload, VerifyCallback } from 'jsonwebtoken'
+import bycrypt from 'bcrypt'
+import { UserInterface } from '../interfaces/UserInterface'
+
+import env from '../config/environment'
+
+const verifyAccessToken = (req: Request, res: Response, next: NextFunction) => {
+  return new Promise((resovle, reject) => {
+    const { token } = req.body
+    if (!token) {
+      throw new Error()
+    }
+    jwt.verify(
+      token,
+      env.jwt.secretKey,
+      (error: jwt.VerifyErrors | null): void => {
+        if (error) {
+          reject(error)
+        }
+      }
+    )
+  })
+}
+
+export { verifyAccessToken }

5208d25bdfd7a45d0333b6c89a3b733939705d5b
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-16 09:47:04 +0300

    Checked db connection to the test env

--- src/controllers/__tests__/UserController.test.ts
+++ src/controllers/__tests__/UserController.test.ts
@@ -1,5 +1,12 @@
+import dbConnection from '../../db/connection'
+
+beforeAll(() => {
+  return dbConnection()
+})
+
 test('dummy test', () => {
   expect(1).toBe(1)
 })
-
-export {}
+afterAll(() => {
+  return dbConnection().then(db => db.connection.close())
+})

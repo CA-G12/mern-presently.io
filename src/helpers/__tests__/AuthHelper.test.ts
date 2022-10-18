7d77001a34e2304d32d3a8d6d383cebddc7b6c84
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-18 16:52:07 +0300

    Added tests

--- src/helpers/__tests__/AuthHelper.test.ts
+++ src/helpers/__tests__/AuthHelper.test.ts
@@ -6,4 +6,11 @@ describe('test auth helpers', () => {
     const result = await AuthHelper.verifyToken(newToken)
     expect(result.id).toBe('newtoken')
   })
+  test('Check password: bcrypt', async () => {
+    const result = await AuthHelper.checkPassword(
+      'Zayan@123',
+      '$2a$10$aO0IxZabCBVVd5InfPiZ9uVbliaRltJhB.RysEqqQb1rwnBldDeDK'
+    )
+    expect(result).toBe(true)
+  })
 })

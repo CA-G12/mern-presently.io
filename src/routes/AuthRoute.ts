84d3d5cedd82b7d63f745438daba42b85f9d5541
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-18 16:42:34 +0300

    Handled authenticate and logout endpoints

--- src/routes/AuthRoute.ts
+++ src/routes/AuthRoute.ts
@@ -3,6 +3,12 @@ import AuthController from '../controllers/AuthController'
 
 const router = express.Router()
 
+router.post('/authenticate', AuthController.login)
 router.post('/token', AuthController.verifyToken)
+router.get('/logout', (req, res) => {
+  res.clearCookie('token').status(200).json({
+    message: 'success'
+  })
+})
 
 export default router

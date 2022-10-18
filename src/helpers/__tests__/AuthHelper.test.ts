412738633d8240a352e86c76c3f732662b83faba
fadezak100 <fadyzaqut@gmail.com>
2022-10-18 16:36:07 +0300

    fix failing tests

--- src/helpers/__tests__/AuthHelper.test.ts
+++ src/helpers/__tests__/AuthHelper.test.ts
@@ -1,7 +1,7 @@
 import AuthHelper from '../AuthHelper'
 
 describe('test auth helpers', () => {
-  it('verifyAccessToken function with valid token', async () => {
+  test('verifyAccessToken function with valid token', async () => {
     const newToken = await AuthHelper.generateAccessToken('newtoken')
     const result = await AuthHelper.verifyToken(newToken)
     expect(result.id).toBe('newtoken')

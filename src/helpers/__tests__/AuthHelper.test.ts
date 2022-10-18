55a5fef78dd0e4785a5ffa55accaa3148e875ebb
fadezak100 <fadyzaqut@gmail.com>
2022-10-18 16:33:07 +0300

    remove dummy test

--- src/helpers/__tests__/AuthHelper.test.ts
+++ src/helpers/__tests__/AuthHelper.test.ts
@@ -1,8 +1,5 @@
 import AuthHelper from '../AuthHelper'
 
-test('dummy', () => {
-  expect(3).toBe(3)
-})
 describe('test auth helpers', () => {
   it('verifyAccessToken function with valid token', async () => {
     const newToken = await AuthHelper.generateAccessToken('newtoken')

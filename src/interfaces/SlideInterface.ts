a74d1e459e2f30405efe1040cc12906541ac3e09
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-19 22:36:55 +0300

    Added an interface for update presentation request

--- src/interfaces/SlideInterface.ts
+++ src/interfaces/SlideInterface.ts
@@ -1,7 +1,13 @@
+import { Request } from 'express'
+
 export interface SlideInterface {
   id: string
   title: string
   link: string
   isPrivate: boolean
   isLive: boolean
+}
+
+export interface UpdatePresentaionRequest extends Request {
+  body: SlideInterface
 }

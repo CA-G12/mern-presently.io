96b91cddf945a94e4e613fe50764fd6ba9ab33bc
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-20 01:03:03 +0300

    Added tests and fixed wrong expectations

--- src/controllers/__tests__/PresentationController.test.ts
+++ src/controllers/__tests__/PresentationController.test.ts
@@ -16,8 +16,8 @@ describe('Updating presentation tests', () => {
     request(app)
       .put('/api/v1/slides/63503d4745241d6bca02ef9c')
       .send({
-        title: 'Data333',
-        link: 'https://hackmd/abd/datastructure.hackmd',
+        title: 'this is the newest title2',
+        link: 'https://hackmd/user/newlink.hackmd',
         isLive: false,
         isPrivate: true
       })
@@ -26,47 +26,46 @@ describe('Updating presentation tests', () => {
         expect(res.status).toBe(200)
         expect(res.body.message).toBe('Edited Successfuly')
         expect(res.body.updatededPresentaion).toEqual({
+          __v: 0,
           _id: '63503d4745241d6bca02ef9c',
-          title: 'Data222',
-          link: 'https://hackmd/abd/datastructure.hackmd',
+          title: 'this is the newest title2',
+          link: 'https://hackmd/user/newlink.hackmd',
           isLive: false,
-          isPrivate: true,
-          __v: 0
+          isPrivate: true
         })
         return done()
       })
   })
-  //TODO: this must pass after handling generic error in PresentationService
-  //   test('Updating a presentation with invalid id', done => {
-  //     request(app)
-  //       .put('/api/v1/slides/700')
-  //       .send({
-  //         title: 'Linked list',
-  //         link: 'https://hackmd/abd/linkedlist.hackmd',
-  //         isLive: false,
-  //         isPrivate: false
-  //       })
-  //       .end((err, res) => {
-  //         if (err) return done()
-  //         expect(res.status).toBe(400)
-  //         return done()
-  //       })
-  //   })
 
-  //TODO: this must pass after adding server error 500 middleware
-  // test('Updating a presentation with invlaid argument type: is Live must be boolean', done => {
-  //   request(app)
-  //     .put('/api/v1/slides/63504041bbc0c96a25b05765')
-  //     .send({
-  //       title: 300,
-  //       link: 'https://hackmd/abd/datastructure.hackmd',
-  //       isLive: 'false',
-  //       isPrivate: true
-  //     })
-  //     .end((err, res) => {
-  //       if (err) return done()
-  //       expect(res.status).toBe(500)
-  //       return done()
-  //     })
-  // })
+  test('Updating a presentation with an invalid id', done => {
+    request(app)
+      .put('/api/v1/slides/63504041bbc0c96a25b05768')
+      .send({
+        title: 'Linked list',
+        link: 'https://hackmd/abd/linkedlist.hackmd',
+        isLive: false,
+        isPrivate: false
+      })
+      .end((err, res) => {
+        if (err) return done()
+        expect(res.status).toBe(400)
+        return done()
+      })
+  })
+
+  test('Updating a presentation with invlaid argument type: is Live must be boolean', done => {
+    request(app)
+      .put('/api/v1/slides/63504041bbc0c96a25b05765')
+      .send({
+        title: 300,
+        link: 'https://hackmd/abd/datastructure.hackmd',
+        isLive: 'string',
+        isPrivate: true
+      })
+      .end((err, res) => {
+        if (err) return done()
+        expect(res.status).toBe(500)
+        return done()
+      })
+  })
 })

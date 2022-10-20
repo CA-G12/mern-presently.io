cedbc91fea39a93580e42d3e09727d532590708e
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-20 10:32:19 +0300

    Served build

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import compression from 'compression'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import { join } from 'path'
 import environment from './config/environment'
 import router from './routes'
 
@@ -17,6 +18,13 @@ app.use([
     credentials: true // access-control-allow-credentials:true
   })
 ])
+
+if (environment.nodeEnv === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')))
+  app.use('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'))
+  })
+}
 
 app.use('/api/v1', router)
 

7536f1c08325675be482c321b3538ef81dde4746
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-23 09:21:43 +0300

    Added seeds for users and slides collections

---
+++ src/db/seeders/users.ts
@@ -0,0 +1,34 @@
+import { UserInterface } from '../../interfaces/UserInterface'
+
+const users: UserInterface[] = [
+  {
+    id: '123ea40720dcfa02e0ae42db',
+    name: 'Ahmed',
+    email: 'ahmed@gmail.com',
+    password: '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
+    slides: []
+  },
+  {
+    id: '456ea40720dcfa02e0ae42db',
+    name: 'Fadi',
+    email: 'fadi@gmail.com',
+    password: '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
+    slides: []
+  },
+  {
+    id: '789ea40720dcfa02e0ae42db',
+    name: 'Zayan',
+    email: 'zayanh@gmail.com',
+    password: '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
+    slides: []
+  },
+  {
+    id: '147ea40720dcfa02e0ae42db',
+    name: 'Nada',
+    email: 'nada@gmail.com',
+    password: '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
+    slides: []
+  }
+]
+
+export = users

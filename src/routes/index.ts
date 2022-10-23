0a1aabce9c2750c77bc006014e8d7f3442cccb94
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2022-10-23 13:22:17 +0300

    added nvmrc

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,6 @@ router.use('/auth', AuthRoute)
 router.use('/slides', SlideRoute)
 router.use('/users', UserRoute)
 
-
 router.use(
   (_error: Error, req: Request, res: Response, _next: NextFunction) => {
     res.status(500).send({ error: 'Internal Server Error' })

1dc71fefd336609c0a09f009ad3c5da1f43ba8d8
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-25 10:13:31 +0300

    Fixed bugs

--- src/db/seeders/SeedDB.ts
+++ src/db/seeders/SeedDB.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import { Seeder } from 'mongo-seeding'
 
 import environment from '../../config/environment'
@@ -14,21 +15,20 @@ const collections = [
     name: 'users',
     documents: [
       {
-        _id: '123ea40720dcfa02e0ae42db',
         name: 'Ahmed',
         email: 'ahmed@gmail.com',
         password:
           '$2a$10$oOcbaZfhAXJzN5hqdD/tquztLXHwQJPUQqbP6f3Ybx521aJDCLBUu',
         slides: [
           {
-            _id: '123ea40720dcfa02e0ae42db',
+            _id: new mongoose.Types.ObjectId('123ea40720dcfa02e0ae42db'),
             title: 'Eslint',
             link: 'https://eslint.org/',
             isPrivate: true,
             isLive: false
           },
           {
-            _id: '456ea40720dcfa02e0ae42db',
+            _id: new mongoose.Types.ObjectId('456ea40720dcfa02e0ae42db'),
             title: 'OAUTH',
             link: 'https://oauth.net/',
             isPrivate: true,
@@ -37,14 +37,13 @@ const collections = [
         ]
       },
       {
-        _id: '456ea40720dcfa02e0ae42db',
         name: 'Fadi',
         email: 'fadi@gmail.com',
         password:
           '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
         slides: [
           {
-            _id: '789ea40720dcfa02e0ae42db',
+            _id: new mongoose.Types.ObjectId('789ea40720dcfa02e0ae42db'),
             title: 'Aligrandpa',
             link: 'https://hackmd.io/j3FxonEgS5GmAukS5gZ1eA',
             isPrivate: true,
@@ -53,14 +52,13 @@ const collections = [
         ]
       },
       {
-        _id: '789ea40720dcfa02e0ae42db',
         name: 'Zayan',
         email: 'zayanh@gmail.com',
         password:
           '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
         slides: [
           {
-            _id: '147ea40720dcfa02e0ae42db',
+            _id: new mongoose.Types.ObjectId('147ea40720dcfa02e0ae42db'),
             title: 'Typescript',
             link: 'https://www.typescriptlang.org/docs/',
             isPrivate: true,
@@ -69,45 +67,12 @@ const collections = [
         ]
       },
       {
-        _id: '147ea40720dcfa02e0ae42db',
+        _id: new mongoose.Types.ObjectId('147ea40720dcfa02e0ae42db'),
         name: 'Nada',
         email: 'nada@gmail.com',
         password:
           '$2a$10$CnZQzaO5CMEW7r4Wdt8uL.d0zefjq26XiTDUbfwsEgqeDVAInf8Si',
         slides: []
-      }
-    ]
-  },
-  {
-    name: 'slides',
-    documents: [
-      {
-        _id: '123ea40720dcfa02e0ae42db',
-        title: 'Eslint',
-        link: 'https://eslint.org/',
-        isPrivate: true,
-        isLive: false
-      },
-      {
-        _id: '456ea40720dcfa02e0ae42db',
-        title: 'OAUTH',
-        link: 'https://oauth.net/',
-        isPrivate: true,
-        isLive: false
-      },
-      {
-        _id: '789ea40720dcfa02e0ae42db',
-        title: 'Aligrandpa',
-        link: 'https://hackmd.io/j3FxonEgS5GmAukS5gZ1eA',
-        isPrivate: true,
-        isLive: false
-      },
-      {
-        _id: '147ea40720dcfa02e0ae42db',
-        title: 'Typescript',
-        link: 'https://www.typescriptlang.org/docs/',
-        isPrivate: true,
-        isLive: false
       }
     ]
   }
@@ -122,10 +87,6 @@ const seed = () => {
     .catch(err => {
       return `Failed to seed the database! ${err}`
     })
-}
-
-if (environment.nodeEnv === 'development') {
-  seed().then(message => console.log(message))
 }
 
 export default seed

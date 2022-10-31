62d1a41fae1fcd83d9c6f981b4afaa8c90a8a37f
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-31 15:37:30 +0200

    Reused react component

--- client/src/components/Slider/index.tsx
+++ client/src/components/Slider/index.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom'
 
 import './styles.css'
 import { useKeyPress } from '../../hooks/useKeyPress'
+import { ReactComponent as LeftArrow } from '../../assets/SlidesIcons/leftArrow.svg'
+import { ReactComponent as RightArrow } from '../../assets/SlidesIcons/rightArrow.svg'
+import { ReactComponent as Home } from '../../assets/SlidesIcons/home.svg'
 
 interface ISliderProps {
   slides: string[]
@@ -38,65 +41,39 @@ const Slider = ({ slides }: ISliderProps) => {
       {/*------------------------ Control slides ------------------------ */}
       <div className="absolute inset-x-0 bottom-0 lg:pr-32 p-4 lg:py-5 lg:pl-32 flex-initial ">
         <div className="justify-self-end flex justify-center">
-          <button onClick={() => navigate('/user')}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              color="grey"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
+          <button
+            onClick={() => navigate('/user')}
+            className="hover:scale-125 focus: outline-none"
+          >
+            <Home strokeWidth={2} height={30} width={40} />
           </button>
           <input
             id="comment"
             className="w-full text-white py-2 px-2 mx-20 my-2 placeholder-grey border-b-2 border-grey-light focus:text-black outline-none"
             placeholder="Add a comment ..."
           />
-          <div className="flex justify-around ">
+          <div className="flex justify-around">
             <button
-              className="mr-10 focus: outline-none"
+              className="mr-10 focus: outline-none hover:scale-125"
               onClick={goToPrevious}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+              <LeftArrow
                 strokeWidth={2}
-                color="grey"
-                className="h-10 w-10 hover:text-primary-default"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 19.5L8.25 12l7.5-7.5"
-                />
-              </svg>
+                height={40}
+                width={40}
+                className="hover:text-primary-default"
+              />
             </button>
-            <button className="focus: outline-none" onClick={goToNext}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            <button
+              className="focus: outline-none hover:scale-125"
+              onClick={goToNext}
+            >
+              <RightArrow
                 strokeWidth={2}
-                color="grey"
-                className="h-10 w-10 hover:text-primary-default"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                />
-              </svg>
+                height={40}
+                width={40}
+                className="hover:text-primary-default"
+              />
             </button>
           </div>
         </div>

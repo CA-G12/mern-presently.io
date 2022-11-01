1dd977d4346ac155d56239615a6eb5fdbcace04a
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-11-01 15:15:42 +0200

    fixed live icon

--- client/src/components/PresentationCard/index.tsx
+++ client/src/components/PresentationCard/index.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as PresentationIcon } from '../../assets/PresentationIcons/presentationIcon.svg'
 import { ReactComponent as LivePresentation } from '../../assets/PresentationIcons/livePresentation.svg'
 import { ReactComponent as DeletePresentation } from '../../assets/PresentationIcons/deletePresentation.svg'
+import { useState } from 'react'
 
 interface IPresentationCardOProps {
   newId: string
@@ -8,6 +9,9 @@ interface IPresentationCardOProps {
 }
 
 const PresentationCard = ({ newId, type }: IPresentationCardOProps) => {
+  const [isPublic, setIsPublic] = useState(false)
+  const [isLive, setIsLive] = useState(false)
+
   return (
     <div className="relative flex flex-col justify-between bg-grey-background w-72 rounded-1">
       <div className="absolute mt-4 right-4">
@@ -16,7 +20,7 @@ const PresentationCard = ({ newId, type }: IPresentationCardOProps) => {
             strokeWidth={2}
             height={20}
             width={20}
-            className="hover:text-danger"
+            className="hover:text-danger hover:scale-125"
           />
         </a>
       </div>
@@ -27,10 +31,23 @@ const PresentationCard = ({ newId, type }: IPresentationCardOProps) => {
       {type === 'uploaded' ? (
         <div className="flex border border-b-0 border-l-0 border-r-0 border-white p-4">
           <div className="flex-1 flex pr-10">
-            <LivePresentation className="mt-1 mr-2" />
-            Live
+            <button className="mr-1" onClick={() => setIsLive(!isLive)}>
+              <LivePresentation
+                strokeWidth={2}
+                height={25}
+                width={25}
+                className={
+                  isLive
+                    ? 'text-primary-default ml-1 scale-125'
+                    : 'text-black  ml-1 hover:scale-125'
+                }
+              />
+            </button>
+            <span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-default">
+              {isLive ? 'Live' : 'not live'}
+            </span>
           </div>
-          <div className="flex-1 flex">
+          <div className="flex justify-between">
             <label
               htmlFor={'default-toggle' + newId}
               className="inline-flex relative items-center cursor-pointer"
@@ -39,10 +56,12 @@ const PresentationCard = ({ newId, type }: IPresentationCardOProps) => {
                 type="checkbox"
                 id={'default-toggle' + newId}
                 className="sr-only peer"
+                checked={isPublic}
+                onChange={() => setIsPublic(!isPublic)}
               />
               <div className="w-11 h-6 bg-grey-default rounded-2 peer-focus:outline-none dark:peer-focus:ring-primary-default peer dark:bg-primary-default peer-checked:after:translate-x-full peer-checked:after:border-grey-default after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-default after:rounded-2 after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-default"></div>
-              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-default">
-                Public
+              <span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-default">
+                {isPublic ? 'Public' : 'Private'}
               </span>
             </label>
           </div>

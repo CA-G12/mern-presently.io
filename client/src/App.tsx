73bd6b0e0c9993fa2e103355679c5b56bec25174
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2022-11-02 07:32:04 +0200

    app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,32 @@
 import { useEffect } from 'react'
 import { useRoutes } from 'react-router-dom'
 import ws from 'socket.io-client'
-
+import useAuth from './hooks/useAuth'
 import Landing from './pages/Landing/Landing'
-import Presentations from './pages/Presentations'
+import Presentations from './pages/Presentation'
+import axios from './api/axios'
+import PublicRoute from './components/PublicRoute'
+import PrivateRoute from './components/PrivateRoute'
+import { UserInterface } from '../../src/interfaces/UserInterface'
+import Login from './components/Login'
 import Presentation from './pages/Presentation'
-import Login from './components/Login/index'
 
 const socket = ws('http://localhost:4000', {
   autoConnect: false
 })
 
 const App = () => {
+  const { dispatch } = useAuth()
   useEffect(() => {
+    ;(async () => {
+      try {
+        const user: UserInterface = await axios.post('/auth/token')
+
+        dispatch && dispatch({ type: 'LOGIN', payload: { user } })
+      } catch (err) {
+        dispatch && dispatch({ type: 'LOGOUT' })
+      }
+    })()
     socket.open()
 
     socket.emit('login', 'UserId has logged in')
@@ -35,11 +49,37 @@ const App = () => {
   const element = useRoutes([
     {
       path: '/',
-      element: <Landing />
+      element: (
+        <PublicRoute>
+          <Landing />
+        </PublicRoute>
+      )
     },
-    { path: '/user', element: <Presentations /> },
-    { path: '/presentation/:id', element: <Presentation /> },
-    { path: '/login', element: <Login /> }
+    {
+      path: '/login',
+      element: (
+        <PublicRoute>
+          <Login />
+        </PublicRoute>
+      )
+    },
+
+    {
+      path: '/user',
+      element: (
+        <PrivateRoute>
+          <Presentations />
+        </PrivateRoute>
+      )
+    },
+    {
+      path: '/presentation/:id',
+      element: (
+        <PrivateRoute>
+          <Presentation />
+        </PrivateRoute>
+      )
+    }
   ])
 
   return element

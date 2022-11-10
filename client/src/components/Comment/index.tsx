c7665c9e294c8838eaf0093f3c7ebb1dfb80a4f1
Fadi Zaqout <fadyzaqut@gmail.com>
2022-11-10 10:18:23 +0200

    Handle Live comments for presentation. (#48)
    * initial socket for comments setup
    
    * fix owner state in the context
    
    * add comments socket
    
    * complete comments socket
    
    * fix bugs

--- client/src/components/Comment/index.tsx
+++ client/src/components/Comment/index.tsx
@@ -4,7 +4,7 @@ interface ICommentProps {
 
 const Comment = ({ text }: ICommentProps) => {
   return (
-    <div className="max-w-sm border-none rounded-1 m-2 p-3 text-justify bg-blue-lightest">
+    <div className="min-w-96 border-none rounded-1 m-2 p-3 text-justify bg-blue-lightest">
       {text}
     </div>
   )

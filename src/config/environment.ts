d41ccd24b0f868dff9df0dd2479311317a121564
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-11-10 15:27:15 +0200

    Edit origin

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -25,7 +25,7 @@ const config = {
   port: PORT || 4000,
   nodeEnv: NODE_ENV || 'development',
   client: {
-    origin: ORIGIN || 'http://localhost:3000'
+    origin: ORIGIN || 'https://presentlyio.netlify.app/'
   },
   jwt: {
     secretKey: SECRET_KEY || ''

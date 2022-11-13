10f3778ec452d2b68b8268a2878463527f3fc5a8
Fadi Zaqout <fadyzaqut@gmail.com>
2022-11-13 13:45:48 +0200

    ws deploy configs (#54)

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,6 @@
 const config = {
-  apiBaseUrl:
-    process.env.REACT_APP_API_URL || 'https://presently.onrender.com//api/v1',
-  wsBaseUrl: process.env.REACT_APP_WS_URL || 'https://presently.onrender.com/'
+  apiBaseUrl: process.env.REACT_APP_API_URL || 'http://localhost:4000/api/v1',
+  wsBaseUrl: process.env.REACT_APP_WS_URL || 'http://localhost:4000'
 }
 
 export default config
